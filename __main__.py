653f8d8090ef4b4474c6cac9e81ea123b97d43c4
Your Name <you@example.com>
2021-07-04 16:30:57 +0530

    creating wordpress using pulumi

---
+++ __main__.py
@@ -0,0 +1,48 @@
+"""A Kubernetes Python Pulumi program"""
+
+import pulumi
+from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
+from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
+from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs
+from pulumi_kubernetes.core.v1 import Service
+
+app_labels = { "app": "wordpress" }
+app_name = "wordpress"
+
+config = pulumi.Config()
+is_minikube = config.require_bool("isMinikube")
+
+deployment = Deployment(
+    "wordpress",
+    spec=DeploymentSpecArgs(
+        selector=LabelSelectorArgs(match_labels=app_labels),
+        replicas=1,
+        template=PodTemplateSpecArgs(
+            metadata=ObjectMetaArgs(labels=app_labels),
+            spec=PodSpecArgs(containers=[ContainerArgs(name="wordpress", image="wordpress")])
+        ),
+    ))
+
+pulumi.export("name", deployment.metadata["name"])
+
+frontend = Service(
+    app_name,
+    metadata={
+        "labels": deployment.spec["template"]["metadata"]["labels"],
+    },
+    spec={
+        "type": "ClusterIP" if is_minikube else "LoadBalancer",
+        "ports": [{ "port": 80, "target_port": 80, "protocol": "TCP" }],
+        "selector": app_labels,
+    })
+
+# When "done", this will print the public IP.
+result = None
+if is_minikube:
+    result = frontend.spec.apply(lambda v: v["cluster_ip"] if "cluster_ip" in v else None)
+else:
+    ingress = frontend.status.apply(lambda v: v["load_balancer"]["ingress"][0] if "load_balancer" in v else None)
+    if ingress is not None:
+        result = ingress.apply(lambda v: v["ip"] if "ip" in v else v["hostname"])
+
+pulumi.export("ip", result)
